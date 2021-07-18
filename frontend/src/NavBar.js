426a816ccf81cc57c4d6a28a727fbdee3de57ad0
sam <wax654@gmail.com>
2021-07-18 14:29:58 -0700

    minor navbar update

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -4,35 +4,27 @@ import UserContext from "./UserContext";
 
 const NavBar = () => {
     const  { user, logoutUser} = useContext(UserContext);
+    const LoggedInLinks = [
+        <NavLink exact to="/" >         Home        </NavLink>,
+        <NavLink exact to="/companies"> companies   </NavLink>,
+        <NavLink exact to="/jobs">      jobs        </NavLink>,
+        <span className="NavBar-greeting"> 
+            Hello, <NavLink exact to="/profile">{user.username}</NavLink>!
+        </span>,
+        <NavLink exact to="/" onClick={logoutUser}>
+                                        logout
+        </NavLink>,
+    ]
+
+    const LoggedOutLinks = [
+        <NavLink exact to = "/" >   Home    </NavLink>,
+        <NavLink exact to="/login"> login   </NavLink>,
+        <NavLink exact to="/signup">Signup  </NavLink>,
+    ]
+
     return (
         <div className="NavBar">
-            
-            <NavLink exact to="/">          Home        </NavLink>
-            <NavLink exact to="/profile">   Profile     </NavLink>
-            <NavLink exact to="/companies"> companies   </NavLink>
-            <NavLink exact to="/jobs">      jobs        </NavLink>
-            {user.username ? 
-                <>  
-                    <span className="NavBar-greeting"> 
-                        Hello, {user.username}! 
-                    </span>
-                    <NavLink exact to="/" onClick={logoutUser}>
-                        logout
-                    </NavLink>
-
-                </> 
-                : 
-                <>
-                    <NavLink exact to="/login">
-                        login
-                    </NavLink>
-                    <NavLink exact to="/signup">
-                        signup
-                    </NavLink>
-                </>
-            }
-           
-
+            {user.username ? LoggedInLinks : LoggedOutLinks}
         </div> 
 )};
 export default NavBar
